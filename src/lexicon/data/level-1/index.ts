b6135c324848d5dcab76dec55506a63f6c243a8f
Roberto Thais <r@robertothais.org>
2018-07-25 00:31:36 +0900

    Add lesson 5

--- src/lexicon/data/level-1/index.ts
+++ src/lexicon/data/level-1/index.ts
@@ -2,5 +2,6 @@ import lesson1 from "./lesson-1.json";
 import lesson2 from "./lesson-2.json";
 import lesson3 from "./lesson-3.json";
 import lesson4 from "./lesson-4.json";
+import lesson5 from "./lesson-5.json";
 
-export default [lesson1, lesson2, lesson3, lesson4];
+export default [lesson1, lesson2, lesson3, lesson4, lesson5];
