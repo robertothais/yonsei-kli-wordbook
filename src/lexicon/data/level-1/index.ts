913ec67ca71ae4e4e429196acee9c567fcf3d0f1
Roberto Thais <r@robertothais.org>
2018-08-08 02:31:26 +0900

    Add lesson 7

--- src/lexicon/data/level-1/index.ts
+++ src/lexicon/data/level-1/index.ts
@@ -4,5 +4,6 @@ import lesson3 from "./lesson-3.json";
 import lesson4 from "./lesson-4.json";
 import lesson5 from "./lesson-5.json";
 import lesson6 from "./lesson-6.json";
+import lesson7 from "./lesson-7.json";
 
-export default [lesson1, lesson2, lesson3, lesson4, lesson5, lesson6];
+export default [lesson1, lesson2, lesson3, lesson4, lesson5, lesson6, lesson7];
